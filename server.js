39cbe71ea98619ac00a2c18fd4efb219c4ca1b27
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2025-04-28 22:43:58 +0300

    don't kill urself just yet

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3030;
 const app = express();
 const mongoose = require("mongoose");
@@ -10,13 +11,17 @@ const connectDB = require("./models/dbConnection");
 const homeRouter = require("./routes/home");
 const authRouter = require("./routes/auth");
 const productsRouter = require("./routes/products");
-const {protect} = require("./middleware/auth.js")
+const { protect } = require("./middleware/auth.js");
+
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
+
 // Mount routers
+
 app.use("/", homeRouter);
 app.use("/auth", authRouter);
-app.use("/products", protect ,productsRouter);
+app.use("/products", productsRouter);
 app.use((req, res) => res.status(404).send("404 Page not found"));
 
 const startServer = async () => {
